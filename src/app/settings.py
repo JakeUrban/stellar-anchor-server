dff5fab10119b3d422761184de7b6e9a93ac6ff7
Jake Urban <10968980+JakeUrban@users.noreply.github.com>
2019-11-19 14:27:53 -0800

    Added whitenoise to middleware (#161)

--- src/app/settings.py
+++ src/app/settings.py
@@ -41,11 +41,13 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "rest_framework",
     "polaris",
+    "app",
 ]
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "corsheaders.middleware.CorsMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
